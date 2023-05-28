580b814489e12ce9ef8397e402bbe1fba5d1fbfa
crownVic0285 <vpcorona85@gmail.com>
2023-05-28 10:28:58 -0700

    makeHtmlBoard updated

--- connect4.js
+++ connect4.js
@@ -26,15 +26,18 @@ makeBoard() {
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
-function makeHtmlBoard() {
+makeHtmlBoard() {
   const board = document.getElementById('board');
+  board.innerHTML = '';
 
   // make column tops (clickable area for adding a piece to that column)
   const top = document.createElement('tr');
   top.setAttribute('id', 'column-top');
-  top.addEventListener('click', handleClick);
 
-  for (let x = 0; x < WIDTH; x++) {
+  this.handleGameClick = this.handleClick.bind(this);
+  top.addEventListener('click', this.handleGameClick);
+
+  for (let x = 0; x < this.width; x++) {
     const headCell = document.createElement('td');
     headCell.setAttribute('id', x);
     top.append(headCell);
@@ -43,10 +46,10 @@ function makeHtmlBoard() {
   board.append(top);
 
   // make main part of board
-  for (let y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < this.height; y++) {
     const row = document.createElement('tr');
 
-    for (let x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < this.height; x++) {
       const cell = document.createElement('td');
       cell.setAttribute('id', `${y}-${x}`);
       row.append(cell);
